e9972debd00141da31572ac3a136a815dc07cbbd
Edge, Matthew <mattedgeis@gmail.com>
2018-03-26 11:59:38 -0400

    Fixing thymeleaf namespace declaration:

--- rhoar-spring-mvc/src/main/java/com/example/service/FruitController.java
+++ rhoar-spring-mvc/src/main/java/com/example/service/FruitController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
-@RequestMapping(value = "/api/fruits")
+@RequestMapping("/api/fruits")
 public class FruitController {
 
     private List<Fruit> fruits = new ArrayList<>();
@@ -15,12 +16,13 @@ public class FruitController {
     @GetMapping
     public String home(Model model) {
         model.addAttribute("fruits", fruits);     // For the List view
-        model.addAttribute("fruit", new Fruit()); // For the Form
+        model.addAttribute("fruitForm", new Fruit()); // For the Form
         return "home";
     }
 
+    // TODO POST mapping here
     @PostMapping
-    public String createFruit(@ModelAttribute Fruit fruit, Model model) {
+    public String createFruit(@ModelAttribute Fruit fruit) {
         fruits.add(fruit);
         return "redirect:/api/fruits";
     }
