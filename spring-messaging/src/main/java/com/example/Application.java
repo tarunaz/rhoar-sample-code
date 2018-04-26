8e060133475b83448e65d35dd28ac900baa41d84
vinnie <vinnie.rossi12@gmail.com>
2018-04-26 11:08:59 -0400

    Adding AMQP connection files

--- spring-messaging/src/main/java/com/example/Application.java
+++ spring-messaging/src/main/java/com/example/Application.java
@@ -13,4 +13,15 @@ public class Application extends WebMvcConfigurerAdapter {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean(name = "amqp-component")
+	AMQPComponent amqpComponent(AMQPConfiguration config) {
+			JmsConnectionFactory qpid = new JmsConnectionFactory(config.getUsername(), config.getPassword(), "amqp://"+ config.getHost() + ":" + config.getPort());
+			qpid.setTopicPrefix("topic://");
+
+			PooledConnectionFactory factory = new PooledConnectionFactory();
+			factory.setConnectionFactory(qpid);
+
+			return new AMQPComponent(factory);
+	}
+
 }
