3c0b4cc68f4444b1e89f9749f465b56308af27df
vinnie <vinnie.rossi12@gmail.com>
2018-04-26 13:22:42 -0400

    Revert attempt at fixing amq

--- spring-messaging/src/main/java/com/example/Application.java
+++ spring-messaging/src/main/java/com/example/Application.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.apache.activemq.jms.pool.PooledConnectionFactory;
-import org.apache.camel.component.amqp.AMQPComponent;
-import org.apache.qpid.jms.JmsConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 
@@ -16,17 +13,6 @@ public class Application extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean(name = "amqp-component")
-	AMQPComponent amqpComponent(AMQPConfiguration config) {
-			JmsConnectionFactory qpid = new JmsConnectionFactory(config.getUsername(), config.getPassword(), "amqp://"+ config.getHost() + ":" + config.getPort());
-			qpid.setTopicPrefix("topic://");
-
-			PooledConnectionFactory factory = new PooledConnectionFactory();
-			factory.setConnectionFactory(qpid);
-
-			return new AMQPComponent(factory);
 	}
 
 }
