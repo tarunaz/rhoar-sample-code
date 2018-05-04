ae43feec8b28f306bed3e67df8e1c27fb1589903
Vinnie <vinnie.rossi12@gmail.com>
2018-05-04 13:29:11 -0400

    fix controller

--- spring-monitoring/src/main/java/com/example/service/HomeController.java
+++ spring-monitoring/src/main/java/com/example/service/HomeController.java
@@ -15,7 +15,7 @@ import static java.util.stream.Collectors.toList;
 @RequestMapping("/")
 public class HomeController {
 
-    private static final Log log = LogFactory.getLog(SampleController.class);
+    private static final Log log = LogFactory.getLog(HomeController.class);
 
     @Autowired
     public HomeController() {
