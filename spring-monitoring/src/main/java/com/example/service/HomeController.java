5b8525c404b26a50cc6c263b44c65955615e67e4
Vinnie <vinnie.rossi12@gmail.com>
2018-05-04 11:38:21 -0400

    Add required imports

--- spring-monitoring/src/main/java/com/example/service/HomeController.java
+++ spring-monitoring/src/main/java/com/example/service/HomeController.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -21,6 +22,8 @@ public class HomeController {
 
     @GetMapping
     public String home() {
+        // TODO: Add Sleuth logging here
+        
         return "home";
     }
 
