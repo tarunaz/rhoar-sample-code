a05807604bf9bb87eb9be7f8c09895ab0074b6f2
Michael Conlon <michael.conlon@levvel.io>
2018-05-31 10:50:00 -0400

    Updated fruitcontroller to log correctly

--- spring-monitoring/src/main/java/com/example/service/FruitController.java
+++ spring-monitoring/src/main/java/com/example/service/FruitController.java
@@ -7,11 +7,18 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 @Controller
 @RequestMapping("/fruits")
 public class FruitController {
 
     private List<Fruit> fruits = new ArrayList<>();
+
+    private static final Log log = LogFactory.getLog(FruitController.class);
 
     @GetMapping
     public String home(Model model) {
